36a072980c034a5e2cbf37561c76620efc2df3e3
Dmitry <git@e965.ru>
2021-07-16 17:34:25 +0500

    client: удалил AppReduxSlice

--- packages/client/src/redux/rootReducer.ts
+++ packages/client/src/redux/rootReducer.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
-import appReducer from '../Components/App/AppReduxSlice';
 import sendPageReducer from '../Components/SendPage/SendPageReduxSlice';
 import adminLoginReducer from '../Components/AdminLogin/AdminLoginReduxSlice';
 
 const rootReducer = combineReducers({
-    app: appReducer,
     sendPage: sendPageReducer,
     adminLogin: adminLoginReducer,
 });
