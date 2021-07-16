231e7f5763ab7d4e6d80653ccd2671302de861ae
Dmitry <git@e965.ru>
2021-07-17 00:02:15 +0500

    server: fix dates calcutation

--- packages/server/src/utils/get-game-stage.ts
+++ packages/server/src/utils/get-game-stage.ts
@@ -5,11 +5,11 @@ const getGameStage = (times: SendFormTimesType): GameStageType | undefined => {
 
     const Now = new Date().getTime();
 
-    const DemoFormStart = new Date(times.demo_end).getTime() - times.open_after * 1000;
-    const DemoFormEnd = new Date(times.demo_end).getTime() + times.open_before * 1000;
+    const DemoFormStart = new Date(times.demo_end).getTime() - times.open_before * 1000;
+    const DemoFormEnd = new Date(times.demo_end).getTime() + times.open_after * 1000;
 
-    const FinalFormStart = new Date(times.final_end).getTime() - times.open_after * 1000;
-    const FinalFormEnd = new Date(times.final_end).getTime() + times.open_before * 1000;
+    const FinalFormStart = new Date(times.final_end).getTime() - times.open_before * 1000;
+    const FinalFormEnd = new Date(times.final_end).getTime() + times.open_after * 1000;
 
     if (DemoFormStart <= Now && Now <= DemoFormEnd) {
         stage = 'demo';
