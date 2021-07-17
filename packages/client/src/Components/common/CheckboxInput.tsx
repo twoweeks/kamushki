c6ebe66b353e7adc098d01448082692cb46e6d9e
Dmitry <git@e965.ru>
2021-07-18 02:03:03 +0500

    client: новая упрощённая вёрстка инпутов
    + доступно больше параметров

--- packages/client/src/Components/common/CheckboxInput.tsx
+++ packages/client/src/Components/common/CheckboxInput.tsx
@@ -2,23 +2,34 @@ import React from 'react';
 
 type PropsTyp = React.InputHTMLAttributes<HTMLInputElement> & {
     id: string;
-    label: string | JSX.Element;
+    label?: string | JSX.Element;
     isLabelHidden?: boolean;
 };
 
 export const CheckboxInput: React.FC<PropsTyp> = props => {
     const { className, id } = props;
     const { label, isLabelHidden } = props;
-    const { required } = props;
+    const { defaultChecked, checked, defaultValue, value } = props;
+    const { onChange } = props;
+    const { required, readOnly, disabled, hidden } = props;
 
     return (
-        <div className={className ? `${className} ${className}--checkbox` : void 0} data-required={required ? '' : null}>
-            <div className={className ? `${className}__input` : void 0} hidden={isLabelHidden ? true : void 0}>
-                <input type="checkbox" name={id} {...{ id, required }} />
-            </div>
-            <div className={className ? `${className}__label` : void 0}>
-                <label htmlFor={id}>{label}</label>
-            </div>
+        <div className={className ? `${className}` : void 0} data-type="checkbox" data-required={required ? '' : null} hidden={hidden}>
+            <input
+                type="checkbox"
+                name={id}
+                className={className ? `${className}__input` : void 0}
+                {...{ id }}
+                {...{ defaultChecked, checked, defaultValue, value }}
+                {...{ onChange }}
+                {...{ required, readOnly, disabled, hidden }}
+            />
+
+            {label ? (
+                <label htmlFor={id} className={className ? `${className}__label` : void 0} hidden={isLabelHidden}>
+                    {label}
+                </label>
+            ) : null}
         </div>
     );
 };
--- packages/client/src/Components/common/TextInput.tsx
+++ packages/client/src/Components/common/TextInput.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 type PropsTyp = React.InputHTMLAttributes<HTMLInputElement> & {
     id: string;
-    label: string | JSX.Element;
+    label?: string | JSX.Element;
     isLabelHidden?: boolean;
     extraText?: string | JSX.Element;
     inputStyle?: React.CSSProperties;
+} & {
+    onTextareaChange?: React.HTMLAttributes<HTMLTextAreaElement>['onChange'];
 };
 
 export const TextInput: React.FC<PropsTyp> = props => {
@@ -13,27 +15,33 @@ export const TextInput: React.FC<PropsTyp> = props => {
     const { label, isLabelHidden, extraText } = props;
     const { type = 'text', placeholder, maxLength } = props;
     const { defaultValue, value } = props;
-    const { required, readOnly } = props;
+    const { onChange, onTextareaChange } = props;
+    const { required, readOnly, disabled, hidden } = props;
     const { inputStyle } = props;
 
+    const UniversalInputProps = {
+        name: id,
+        style: inputStyle,
+        className: className ? `${className}__input` : void 0,
+        ...{ id, type, placeholder, maxLength },
+        ...{ defaultValue, value },
+        ...{ required, readOnly, disabled, hidden },
+    };
+
     return (
-        <div className={className ? ` ${className} ${className}--${type}` : void 0} data-required={required ? '' : null}>
-            <div className={className ? `${className}__label` : void 0} hidden={isLabelHidden ? true : void 0}>
-                <label htmlFor={id}>{label}</label>
-            </div>
-            <div className={className ? `${className}__input` : void 0}>
-                {type === 'textarea' ? (
-                    <textarea name={id} style={inputStyle} {...{ id, placeholder, maxLength, required }} />
-                ) : (
-                    <input
-                        name={id}
-                        style={inputStyle}
-                        {...{ id, type, placeholder, maxLength }}
-                        {...{ defaultValue, value }}
-                        {...{ required, readOnly }}
-                    />
-                )}
-            </div>
+        <div className={className ? ` ${className}` : void 0} data-type={type} data-required={required ? '' : null} hidden={hidden}>
+            {label ? (
+                <label htmlFor={id} className={className ? `${className}__label` : void 0} hidden={isLabelHidden}>
+                    {label}
+                </label>
+            ) : null}
+
+            {type === 'textarea' ? (
+                <textarea {...UniversalInputProps} onChange={onTextareaChange} />
+            ) : (
+                <input {...UniversalInputProps} {...{ onChange }} />
+            )}
+
             {extraText ? <div className={`${className}__extraText`}>{extraText}</div> : null}
         </div>
     );
