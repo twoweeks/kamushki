30bb833d98302794ed111507c2a0ae3ad7530476
Dmitry <git@e965.ru>
2021-07-25 23:11:40 +0500

    client: плагин для создания легаси-билда
    потому что много кто жаловался, что форма не работает

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import reactJsx from 'vite-react-jsx';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './',
-    plugins: [reactRefresh(), reactJsx()],
+    plugins: [reactRefresh(), reactJsx(), legacy({ targets: ['defaults', 'not IE 11'] })],
 
     server: {
         proxy: {
